bcf8657513eeadb6fc09aa98323d5817208c31a4
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-22 17:40:51 +0900

    [Update] - Updated permutation.py file

--- permutation.py
+++ permutation.py
@@ -1,11 +1,22 @@
+# 1. 중복 없음 -> 정렬 우선 진행
+# 2. 한 번씩만 있을 경우 = True
+# 3. 여러 개 있거나 없을 경우 = False
+
 def solution(arr):
-    arr.sort()
-    
+    # answer = True
+
     for i in range(len(arr)):
-        if i + 1 != arr[i]:
+        if (i + 1 != arr[i]):
+            # print("True")
+            # print("False")
             return False
-            
+            # return False
+
+    # print("True")
     return True
+    # print(arr[i])
+
+    # return answer
 
 
 if __name__ == "__main__":
