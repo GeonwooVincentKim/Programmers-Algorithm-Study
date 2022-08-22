4f58bb54b71ec05cd82b4d2f47ddd29087d04784
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-22 22:31:15 +0900

    [Feature] - Added new permutation test file

---
+++ extra/permutation_test.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    # answer = True
+
+    for i in range(len(arr)):
+        print(i + 1)
+    
+    print(max(arr))
+
+    # return answer
+
+
+if __name__ == "__main__":
+    print(solution([4, 1, 3, 2]))
