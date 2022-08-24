fdc5a0aadf4252ed501087df121308c4b47fc001
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-24 11:30:47 +0900

    [Feature] - Added level2 problem

---
+++ skill_level/level_2/level2_rectangle_tile.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = 0
+
+    dp = [0] * 5001
+    dp[0] = 1
+    sub = 0
+
+    for i in range(2, n + 1, 2):
+        dp[i] = dp[i - 2] * 3 + sub * 2
+        sub += dp[i - 2]
+
+    answer = dp[n] % 1000000007
+    return answer
+
+
+if __name__ == "__main__":
+    print(solution(8))
