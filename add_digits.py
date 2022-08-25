c6cc5487c21994823fb17c0c52263ca5107beb2e
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-25 11:28:47 +0900

    [Update] - Updated add-digits

--- add_digits.py
+++ add_digits.py
@@ -1,13 +1,12 @@
 def solution(n):
     sum = 0
-    a = int(n)
 
-    while(a != 0):
-        sum += (a % 10)
-        n = a / 10
+    while(int(n) != 0):
+        sum += (int(n) % 10)
+        n = int(n) / 10
     
     return int(sum)
 
 
 if __name__ == "__main__":
-    print(solution(328))
+    print(solution(123))
