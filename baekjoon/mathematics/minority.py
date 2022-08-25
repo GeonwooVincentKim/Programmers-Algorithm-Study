a406f61e53524bfb6345d595a1dfe0c327e29fe4
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-25 14:21:35 +0900

    [Update] - changed `a` to `i`

--- baekjoon/mathematics/minority.py
+++ baekjoon/mathematics/minority.py
@@ -4,13 +4,14 @@ b = int(input())
 sum = 0
 minority = []
 
-for i in range(a, b):
+for i in range(a, b + 1):
     result = True
 
     if i > 1:
-        for j in range(2, a):
+        for j in range(2, i):
             if (i % j == 0):
                 result = False
+                break
         
         if(result):
             minority.append(i)
