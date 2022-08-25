ae02a622501e427b617aff3bfa26cba01935ee14
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-25 17:00:55 +0900

    [Update] - Updated codes

--- baekjoon/dp/easy_to_solve.py
+++ baekjoon/dp/easy_to_solve.py
@@ -1,7 +1,16 @@
-a, b = map(int, input().split())
+# a, b = map(int, input().split())
 
-sum = 0
-for i in range(a + 1, b):
-    sum += i
+# sum = 0
+# for i in range(a + 1, b):
+#     sum += i
 
-print(sum)
+# print(sum)
+
+a,b = map(int,input().split())
+ 
+arr = [0]
+for i in range(46):
+    for j in range(i):
+        arr.append(i)
+ 
+print(sum(arr[a:b+1]))
