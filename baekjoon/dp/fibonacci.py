5e2fc894e0b4fc8d02518bd74d347e07128dc62e
GeonwooVincentKim <kdsnop@gmail.com>
2022-08-28 22:19:04 +0900

    [Update] - Fibonacci has been updated

--- baekjoon/dp/fibonacci.py
+++ baekjoon/dp/fibonacci.py
@@ -1,3 +1,21 @@
-a = int(input())
+def fib(n):
+    if (n == 1 or n == 2):
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+    
 
-dp = [0] * 23000
+def fibonacci(a):
+    dp = [0] * (a + 1)
+    dp[1], dp[2] = 1, 1
+    count = 0
+
+    for i in range(3, a + 1):
+        count += 1
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return count
+
+
+if __name__ == "__main__":
+    a = int(input())
+    print(fib(a), fibonacci(a))
