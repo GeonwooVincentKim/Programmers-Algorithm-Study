6a8ea6964f377a0f201ebc58de0f6a721ed84651
GeonwooVincentKim <kdsnop@gmail.com>
2022-09-05 22:30:24 +0900

    [Feature] - N_M_1.py

---
+++ baekjoon/back_tracking/n_m_1.py
@@ -0,0 +1,18 @@
+a, b = map(int, input().split())
+
+array = []
+
+
+def backtracker_array(k, n, m):
+    if k == m:
+        print(' '.join(map(str, array)))
+        return
+
+    for i in range(1, n + 1):
+        if i not in array:
+            array.append(i)
+            backtracker_array(k + 1, n, m)
+            array.pop()
+
+
+backtracker_array(0, a, b)
