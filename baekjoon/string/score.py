4204dc565d453590695340d6702bb13323614f3b
GeonwooVincentKim <kdsnop@gmail.com>
2022-09-08 22:29:39 +0900

    [Feature] - Score.py has been added

---
+++ baekjoon/string/score.py
@@ -0,0 +1,29 @@
+a = input()
+
+if a in 'A0':
+    print(4.0)
+if a in 'A+':
+    print(4.3)
+if a in 'A-':
+    print(3.7)
+if a in 'B+':
+    print(3.3)
+if a in 'B0':
+    print(3.0)
+if a in 'B-':
+    print(2.7)
+if a in 'C+':
+    print(2.3)
+if a in 'C0':
+    print(2.0)
+if a in 'C-':
+    print(1.7)
+if a in 'D+':
+    print(1.3)
+if a in 'D0':
+    print(1.0)
+if a in 'D-':
+    print(0.7)
+if a in 'F':
+    print(0.0)
+
