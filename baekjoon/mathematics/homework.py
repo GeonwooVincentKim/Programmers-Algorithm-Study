562cdae5da48ec49b0c893fca3f0775e973d42d7
GeonwooVincentKim <kdsnop@gmail.com>
2022-09-10 22:26:19 +0900

    [Update] - Rollback the codes

--- baekjoon/mathematics/homework.py
+++ baekjoon/mathematics/homework.py
@@ -1,12 +1,12 @@
 total = []
-for i in range(1, 7):
+for i in range(1, 31):
     total.append(i)
 
 value = []
-for i in range(6):
+for i in range(28):
     a = int(input())
     value.append(a)
 
 for i in total:
     if i not in value:
-        print("Print -> {0}".format(i))
+        print(i)
