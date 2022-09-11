ed523273dbd4790ed6e4aa47660433aa2c8e860a
GeonwooVincentKim <kdsnop@gmail.com>
2022-09-11 19:29:17 +0900

    [Feature] - Find Minority.py has been solved

---
+++ baekjoon/mathematics/find_minority.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = list(map(int, input().split()))
+    
+count = 0
+for i in b:
+    check = 0
+
+    if i == 1:
+        continue
+
+    for j in range(2, i):
+        if (i % j == 0):
+            check = 1
+    
+    if check == 0:
+        count += 1
+
+print(count)
