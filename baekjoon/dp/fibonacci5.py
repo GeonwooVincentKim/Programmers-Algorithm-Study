542240883d876268cf1d7bef6c2ff3bcac06305a
GeonwooVincentKim <kdsnop@gmail.com>
2022-09-11 21:16:31 +0900

    [Update] - Updated fibonacci5.py codes

--- baekjoon/dp/fibonacci5.py
+++ baekjoon/dp/fibonacci5.py
@@ -1,9 +1,7 @@
 def fibo(n):
-    if n == 1 or n == 2:
-        return 1
-    
-    else:
-        return fibo(n - 1) + fibo(n - 2)
+    if n <= 1:
+        return n
+    return fibo(n - 1) + fibo(n - 2)
 
 
 if __name__ == "__main__":
