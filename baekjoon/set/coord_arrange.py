a097380e9decd32c8245d7b8a03b68aa06151147
GeonwooVincentKim <kdsnop@gmail.com>
2022-09-15 21:33:14 +0900

    [Feature] - Coord-Arrange.py has been added

---
+++ baekjoon/set/coord_arrange.py
@@ -0,0 +1,16 @@
+a = int(input())
+
+b_list = []
+
+for i in range(a):
+    b_list.append(list(map(int, input().split())))
+b_list.sort()
+
+for j in b_list:
+    print("{0} {1}".format(j[0], j[1]))
+
+# 1. Input the Matrix as the number of a
+# 2. Store each Matrix into new List
+# 3. Split Matrix x and y
+# 4. Compare value x from each Matrix
+# 5. If value x has a same value, compare value y
