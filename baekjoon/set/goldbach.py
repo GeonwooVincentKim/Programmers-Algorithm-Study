029593fe60b617433d218d778288c9cf934317ba
GeonwooVincentKim <kdsnop@gmail.com>
2022-09-16 21:10:10 +0900

    [Feature] - GoldBach.py has been added

---
+++ baekjoon/set/goldbach.py
@@ -0,0 +1,29 @@
+def isPrime(n):
+    if n == 1:
+        return False
+    for j in range(2, int(n**0.5) + 1):
+        if n % j == 0:
+            return False
+    return True
+
+
+for _ in range(int(input())):
+    num = int(input())
+
+    a, b = num//2, num//2
+    while a > 0:
+        if isPrime(a) and isPrime(b):
+            print(a, b)
+            break
+        else:
+            a -= 1
+            b += 1
+# a = int(input())
+
+# for i in range(2, a):
+#     if (a % i == 0):
+#         print()
+    
+
+# print(a)
+
