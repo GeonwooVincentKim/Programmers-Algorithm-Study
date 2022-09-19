0718a448a8c5331887e8eaf8ba647df1617223bd
GeonwooVincentKim <kdsnop@gmail.com>
2022-09-19 21:22:18 +0900

    [Feature] - Queue.py has been modified

--- baekjoon/queue/queue.py
+++ baekjoon/queue/queue.py
@@ -1,7 +1,38 @@
 import sys
 
 a = int(input())
+queue_list = []
 
 for i in range(a):
-    value = sys.stdin.readline()
+    value = sys.stdin.readline().split()
+    order = value[0]
+
+    if (order == 'push'):
+        queue_list.insert(0, value[1])
     
+    elif (order == 'pop'):
+        if (len(queue_list) == 0):
+            print(-1)
+        else:
+            print(queue_list.pop())
+    
+    elif (order == 'size'):
+        print(len(queue_list))
+    
+    elif (order == 'empty'):
+        if (len(queue_list) == 0):
+            print(1)
+        else:
+            print(0)
+    
+    elif (order == 'front'):
+        if (len(queue_list) == 0):
+            print(-1)
+        else:
+            print(queue_list[len(queue_list) - 1])
+
+    elif (order == 'back'):
+        if (len(queue_list) == 0):
+            print(-1)
+        else:
+            print(queue_list[0])
