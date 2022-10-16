bba93ee5822f594439a3deb252f1e091fa1fb122
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-16 20:23:23 +0900

    [Update] - Updated codes

--- baekjoon/back_tracking/n_m_2.py
+++ baekjoon/back_tracking/n_m_2.py
@@ -1,17 +1,17 @@
-n, m = map(int, input().split())
+a, b = map(int, input().split())
 
 array = []
 
-def backtracker_array(start):
-    if len(array) == m:
+def backtracker_array(k, n, m , start):
+    if k == m:
         print(' '.join(map(str, array)))
         return
 
     for i in range(start, n + 1):
         if i not in array:
             array.append(i)
-            backtracker_array(i + 1)
+            backtracker_array(k + 1, n, m, i + 1)
             array.pop()
 
 
-backtracker_array(1)
+backtracker_array(0, a, b, 1)
