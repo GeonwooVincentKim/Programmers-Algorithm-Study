2f1e1cc3678d72f9730733435bb09d142d7dea03
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-26 15:48:03 +0900

    [Update] - Color Paper New.py

--- baekjoon/basic/color_paper_new.py
+++ baekjoon/basic/color_paper_new.py
@@ -1,19 +1,21 @@
 n = int(input())
-
-list = [[0] * 100 for _ in range(100)]
+color_paper_list = [[0 for _ in range(100)] for _ in range(100)]
 
 for _ in range(n):
-    a, b = map(int, input().split())
+    x, y = map(int, input().split())
+    x1 = x + 10
+    y1 = y + 10
 
-    for i in range(a, a + 10):
-        for j in range(b, b + 10):
-            list[i][j] = 1
+    for i in range(x, x1):
+        for j in range(y, y1):
+            color_paper_list[i][j] = '*'
+
 
 count = 0
-for row in list:
-    count += row.count(1)
-
+for row in color_paper_list:
+    count += row.count('*')
 print(count)
+
 
 """
 1. Input the number of Color Paper
@@ -36,4 +38,10 @@ print(count)
     -> 
   = 3. (y[2][0] - y[0][0])
   = 4. (y[2][1] - y[0][1])
+
+6. Fill an overlap between several Rectangle
+- 1. If it's overlap, set the value as 1, which shows black color, and count the number of overlap area and its size
+- 2. Otherwise, it won't count the number of overlap area and its size
+
+7. Count `1` which are overlap, and save into count variable
 """
