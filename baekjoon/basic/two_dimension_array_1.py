e6e7b1ad7beefd1458f9ed3d497e4e5c3e4bfa69
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-26 10:53:46 +0900

    [Update] - Updated codes

--- baekjoon/basic/two_dimension_array_1.py
+++ baekjoon/basic/two_dimension_array_1.py
@@ -1,12 +1,8 @@
 n = 9
-
-# mylist = [list(map(int, input().split())) for _ in range(n)]
 x = y = 0
 max_value = -1
 
-mylist = [0 for _ in range(n)]
-for i in range(n):
-    mylist[i] = list(map(int, input().split()))
+mylist = [list(map(int, input().split())) for _ in range(n)]
 
 for i in range(n):
     for j in range(n):
