d7efa04090454bc2f3e007220e16c88406cb5b0b
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-26 17:16:51 +0900

    [Feature] - Palindrome.py

---
+++ baekjoon/string/check_palindrome.py
@@ -0,0 +1,25 @@
+from itertools import count
+
+
+def recursion(s, l, r):
+    global count
+    count += 1
+
+    if (l >= r):
+        return 1
+    elif (s[l] != s[r]):
+        return 0
+    else:
+        return recursion(s, l + 1, r - 1)
+
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s) - 1)
+
+
+a = int(input())
+
+for i in range(a):
+    count = 0
+    print(isPalindrome(input().rstrip()), count)
+
