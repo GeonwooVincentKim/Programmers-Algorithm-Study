5760a3ceb66ee292ee175ac522b3c4c742a55cb6
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-26 10:50:58 +0900

    [Feature] - Two Dimension Array

---
+++ baekjoon/basic/two_dimension_array_1.py
@@ -0,0 +1,18 @@
+n = 9
+
+x = y = 0
+max_value = 0
+
+mylist = [0 for _ in range(n)]
+for i in range(n):
+    mylist[i] = list(map(int, input().split()))
+
+for i in range(n):
+    for j in range(n):
+        if mylist[i][j] > max_value:
+            max_value = mylist[i][j]
+            x = i + 1
+            y = j + 1
+
+print(max_value)
+print(x, y)
