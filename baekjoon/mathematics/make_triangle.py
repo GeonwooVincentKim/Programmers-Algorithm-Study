e8b2ea8dcebb728ba8194f87113b332daa418e11
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-27 14:07:08 +0900

    [Feature] - Make Triangle

---
+++ baekjoon/mathematics/make_triangle.py
@@ -0,0 +1,30 @@
+a = int(input())
+count = 0
+
+for x in range(1, a):
+    for y in range(x, a):
+        z = a - (x + y)
+
+        if (x + y > z) and (x + z > y) and (y + z > x) and y <= z:
+            count += 1
+
+        if (y > z):
+            break
+
+print(count)
+
+"""
+1. An isoceles triangle
+- 1. The two sides of triangle are equal in length
+- 2. The two sides triangle sum should be bigger than other side of triangle length
+
+2. A right-angled triangle
+- Pythagorean theorem
+
+3. Regular triangle
+- Every side of triangle shoudl equal in length
+
+**
+1. Get the other side of triangle length
+2. 
+"""
