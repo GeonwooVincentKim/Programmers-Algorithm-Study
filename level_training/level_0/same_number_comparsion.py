f7612b58dac9cd88bf56f5b3d57866940bc614b6
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-28 16:30:28 +0900

    [Feature] - Same number

---
+++ level_training/level_0/same_number_comparsion.py
@@ -0,0 +1,11 @@
+def solution(num1, num2):
+    if (num1 == num2):
+        return 1
+    else:
+        return -1
+
+
+if __name__ == "__main__":
+    print(solution(2, 3))
+    print(solution(11, 11))
+    print(solution(7, 99))
