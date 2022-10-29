9522f52bf6e24cd01a302e4e16a0658c1a4d3259
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-29 23:24:06 +0900

    [Update] - Checking

--- baekjoon/brute_force/chessboard.py
+++ baekjoon/brute_force/chessboard.py
@@ -1,29 +1,27 @@
-n, m = map(int, input().split())
+n,m=map(int,input().split())
 
-board = result = []
-
-for _ in range(n):
-    board.append(input())
-
-for i in range(n - 7):
-    for j in range(m - 7):
-        draw1 = draw2 = 0
-
-        for a in range(i, i + 8):
-            for b in range(j, j + 8):
-                if (a + b) % 2 == 0:
-                    if board[a][b] != 'B':
-                        draw1 += 1
-                    if board[a][b] != 'W':
-                        draw2 += 1
-                
-                else:
-                    if board[a][b] != 'W':
-                        draw1 += 1
-                    if board[a][b] != 'B':
-                        draw2 += 1
-
-        result.append(draw1)
-        result.append(draw2)
-
-print(min(result))
+mtr=[]
+cnt=[]
+for i in range(n):
+    mtr.append(input())
+    
+for a in range(n-7):
+    for b in range(m-7):#8*8로 자르기 위해, -7해준다
+        w_index=0 #흰색으로 시작
+        b_index=0 #검은색으로 시작
+        for i in range(a,a+8):#시작지점
+            for j in range(b,b+8):#시작지점
+                if (i+j)%2==0:#짝수인 경우
+                    if mtr[i][j]!='W':#W가 아니면, 즉 B이면
+                        w_index+=1#W로 칠하는 갯수
+                    else:#W일 때
+                        b_index+=1#B로 칠하는 갯수
+                else:#홀수인 경우
+                    if mtr[i][j]!='W':#W가 아니면, 즉 B이면
+                        b_index+=1#B로 칠하는 갯수
+                    else:
+                        w_index+=1#W로 칠하는 갯수
+                        
+        cnt.append(w_index) #W로 시작할 때 경우의 수
+        cnt.append(b_index) #B로 시작할 때 경우의 수
+print(min(cnt))
