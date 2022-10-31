82077a0a504ff95a36497328ad61403ad6a7c18e
GeonwooVincentKim <kdsnop@gmail.com>
2022-10-31 11:21:44 +0900

    [Feature] - Matrix Multiplication.py

--- baekjoon/mathematics/matrix/matrix_multiplication.py
+++ baekjoon/mathematics/matrix/matrix_multiplication.py
@@ -1,30 +1,24 @@
-def solution(a, b):
-    n = len(a)
-    multi_array = [[0] * n for _ in range(n)]
+N, M = map(int, input().split())
 
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                multi_array[i][j] += a[i][k] * b[k][j]
+a_list = []
+for i in range(N):
+    a_list.append(list(map(int, input().split())))
 
-    return multi_array
+M, K = map(int, input().split())
+
+b_list = []
+for i in range(M):
+    b_list.append(list(map(int, input().split())))
 
 
-if __name__ == "__main__":
-    a_list_col, a_list_row = map(int, input().split())
+answer_list = [[0 for _ in range(K)] for _ in range(N)]
 
-    a_list = []
-    for i in range(a_list_col):
-        a_list.append(list(map(int, input().split())))
+for i in range(N):
+    for j in range(K):
+        for k in range(M):
+            answer_list[i][j] += a_list[i][k] * b_list[k][j]
 
-    b_list_col, b_list_row = map(int, input().split())
-
-    b_list = []
-    for i in range(b_list_col):
-        b_list.append(list(map(int, input().split())))
-
-    answer = solution(a_list, b_list)
-    print(answer)
-    # a = list(map(int, input().split()))
-    # b = list(map(int, map(int, input().split())))
-    # print(a, b)
+for i in answer_list:
+    for j in i:
+        print(j, end=' ')
+    print()
