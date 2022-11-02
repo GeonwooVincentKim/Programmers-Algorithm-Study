f161d73f5f535bf4fccbba77d0c0d5bfa658e198
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-02 17:11:16 +0900

    [Update] - Updated codes

--- baekjoon/mathematics/distributed_processing.py
+++ baekjoon/mathematics/distributed_processing.py
@@ -3,23 +3,25 @@ a = int(input())
 for i in range(a):
     n, m = map(int, input().split())
     n = n % 10
-    
+
     if n == 0:
         print(10)
-
     elif n == 1 or n == 5 or n == 6:
         print(n)
-
+    
     elif n == 4 or n == 9:
-        m = m % 2
+        m %= 2
+
         if m == 1:
             print(n)
         else:
             print((n * n) % 10)
-
+    
     else:
-        m = m % 4
+        m %= 4
         if m == 0:
             print((n ** 4) % 10 % 10 % 10)
         else:
             print((n ** m) % 10 % 10 % 10)
+
+    
