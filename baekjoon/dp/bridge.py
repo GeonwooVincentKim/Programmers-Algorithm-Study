3ffb47084378122fa770b156d0da4d3db42890ff
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-08 21:52:15 +0900

    [Feature] - Bridge.py

--- baekjoon/dp/bridge.py
+++ baekjoon/dp/bridge.py
@@ -3,5 +3,5 @@ import math
 a = int(input())
 
 for i in range(a):
-    inner_list = list(map(int, input().split()))
-    print(math.comb(inner_list[0], inner_list[1]))
+    get_value = list(map(int, input().split()))
+    print(math.comb(get_value[1], get_value[0]))
