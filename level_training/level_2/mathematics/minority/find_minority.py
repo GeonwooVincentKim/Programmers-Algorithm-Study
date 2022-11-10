f0ead65393b8faa8666c1e71daecef97fd258e12
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-10 11:09:26 +0900

    [Feature] - Find Minority.py

--- level_training/level_2/mathematics/minority/find_minority.py
+++ level_training/level_2/mathematics/minority/find_minority.py
@@ -1,10 +1,32 @@
-def solution(numbers):
-    number_string_list = list(numbers)
+from itertools import permutations
 
+
+def solution(numbers):
     answer = 0
+    case = makeCase(numbers)
+    for n in case:
+        if isPrime(n):
+            answer += 1
+
     return answer
 
 
+def makeCase(numbers):
+    result = []
+    for i in range(1, len(numbers)+1):
+        result.extend(list(map(int, map(''.join, permutations(numbers, i)))))
+    result = list(filter(lambda x: x > 1, list(set(result))))
+
+    return result
+
+
+def isPrime(n):
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
 if __name__ == "__main__":
-    solution("17")
-    # solution("011")
+    print(solution("17"))
+    print(solution("011"))
