1a791bcceb6b50cd9465f811016737517c2e2fe9
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-10 09:57:59 +0900

    [Feature] - DP with plus123.py

---
+++ baekjoon/dp/plus123.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+dp = [0] * 11
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 11):
+    dp[i] = sum(dp[i - 3:i])
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
