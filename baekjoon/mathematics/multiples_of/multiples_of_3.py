123c245e40c3761726597c122a3c5951cf619a02
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-10 11:46:21 +0900

    [Update] - Updated codes

--- baekjoon/mathematics/multiples_of/multiples_of_3.py
+++ baekjoon/mathematics/multiples_of/multiples_of_3.py
@@ -1,31 +1,19 @@
-a = int(input())
-value_list = list(map(int, str(a)))
-# print(value_list)
-
-count = 0
-while True:
-    if len(value_list) == 1:
-        inner_sum = value_list[0]
-        break
-
-    count += 1
-
-    inner_sum = 0
-    for i in value_list:
-        inner_sum += i
-
-    if inner_sum < 10:
-        break
+def func(string, cnt):
+    if len(string) > 1:
+        cnt += 1
+        t = 0
+        for i in string:
+            t += int(i)
+        func(str(t), cnt)
     else:
-        value_list = list(map(int, str(inner_sum)))
+        if int(string) % 3 == 0:
+            print(cnt)
+            print("YES")
+        else:
+            print(cnt)
+            print("NO")
 
-if count == 3 or count == 6 or count == 9:
-    print(i)
-    print("YES")
-else:
-    print(i)
-    print("NO")
 
-# count = 0
-# while True:
-#     if a % 3 == 0:
+n = input()
+cnt = 0
+func(n, cnt)
