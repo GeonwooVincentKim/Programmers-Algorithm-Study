01c91da2117d8c7b6eb37de09d7b2033ce0763ee
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-14 15:38:20 +0900

    [Update] - Updated codes

--- baekjoon/game_theory.py/aliquot_game.py
+++ baekjoon/game_theory.py/aliquot_game.py
@@ -6,7 +6,7 @@ n = int(sys.stdin.readline())
 division_num = 0
 
 get_aliquot_list = []
-for i in range(2, round(float(n + 1))):
+for i in range(2, int(math.sqrt(n)) + 1):
     if n % i == 0:
         division_num += 1
         if (division_num > 1):
