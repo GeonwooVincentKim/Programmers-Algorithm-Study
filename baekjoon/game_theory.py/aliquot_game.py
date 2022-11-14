05dd2a85a21fdab7d322dab81cac4607f16cebc6
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-14 15:21:44 +0900

    [Update] - Old Version

--- baekjoon/game_theory.py/aliquot_game.py
+++ baekjoon/game_theory.py/aliquot_game.py
@@ -3,43 +3,22 @@ import math
 
 n = int(sys.stdin.readline())
 
+division_num = 0
+
 get_aliquot_list = []
-
-count = 0
-while n and count <= 2:
-    flag = False
-
-    for i in range(2, n):
-        if (not (n % i)):
-            count += 1
-            n /= i
-            flag = True
+for i in range(2, n + 1):
+    if n % i == 0:
+        division_num += 1
+        if (division_num > 1):
             break
-    
-    if n == 1:  break
-    elif (not flag):    
-        count += 1  
-        break
 
-if (count == 0 or count == 1 or count == 3):
-    print("koosaga")
-else:
+        n /= i
+        i -= 1
+
+
+if division_num == 1:
     print("cubelover")
-
-
-# for i in range(2, i * i):
-#     if n % i == 0:
-#         division_num += 1
-#         if (division_num > 1):
-#             break
-
-        # n /= i
-        # i -= 1
-
-
-# if division_num == 1:
-#     print("cubelover")
-# else:
-#     print("koosaga")
+else:
+    print("koosaga")
 
 # print(get_aliquot_list)
