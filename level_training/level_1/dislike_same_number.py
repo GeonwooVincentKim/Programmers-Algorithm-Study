55d82430373ab90f2a666c10a0c38bbcad00d184
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-15 15:12:17 +0900

    [Feature] - Dislike same number.py

---
+++ level_training/level_1/dislike_same_number.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    answer = []
+    
+    for i in arr:
+        if answer[-1:] == [i]:   continue
+        answer.append(i)
+    
+    return answer
+
+
+if __name__ == "__main__":
+    print(solution([1, 1, 3, 3, 0, 1, 1]))
+    print(solution([4, 4, 4, 3, 3]))
