df50a35560e2557ee5cfd5105aa736dbb08b1d71
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-15 14:38:34 +0900

    [Update] - Updated divide.py

--- level_training/level_0/divide.py
+++ level_training/level_0/divide.py
@@ -1,6 +1,8 @@
 def solution(num1, num2):
-    result = num1 / num2 * 1000
-    return round(result)
+    result = int(num1 / num2 * 1000)
+    return result
 
 if __name__ == "__main__":
     print(solution(3, 2))
+    print(solution(7, 3))
+    print(solution(1, 16))
