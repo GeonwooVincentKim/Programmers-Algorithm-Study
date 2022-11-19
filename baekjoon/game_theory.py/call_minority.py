e4ab3f4e3a6daed1d455f2e8139da8e0e13f4ad5
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-19 22:58:24 +0900

    [Feature] - Solved problem

--- baekjoon/game_theory.py/call_minority.py
+++ baekjoon/game_theory.py/call_minority.py
@@ -17,30 +17,29 @@ def get_value(n, m, value_list):
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
-# check_list = []
 check_list_yt = []
 check_list_yj = []
 
 get_value(a, b, check_list_yt)
 get_value(c, d, check_list_yj)
 
-# get_value(c, d, check_list_yj)
-print("check_list Yoontae -> {0}".format(check_list_yt))
-print("check_list Yujin -> {0}".format(check_list_yj))
+merge_list = []
 
+for i in check_list_yt:
+    merge_list.append(i)
 
-# set_list_yt = set(check_list_yt)
-# set_list_yj = set(check_list_yj)
-# sorted(set_list_yj)
+for j in check_list_yj:
+    merge_list.append(j)
 
-# print("set_list_yt -> {0}".format(set_list_yt))
-# print("set_list_yj -> {0}".format(set_list_yj))
-# if len(set_list_yt) == len(set_list_yj):
+if len(check_list_yt) == len(check_list_yj):
+    set_merge_list = set(merge_list)
 
+    if len(set_merge_list) % 2 == 0:
+        print('yj')
+    else:
+        print('yt')
 
-# if len(set_list) % 2 == 0:
-#     print('yj')
-# elif a == 2 and b == 3 and c == 5 and d == 11:
-#     print('yj')
-# else:
-#     print('yt')
+elif len(check_list_yt) > len(check_list_yj):
+    print('yt')
+elif len(check_list_yt) < len(check_list_yj):
+    print('yj')
