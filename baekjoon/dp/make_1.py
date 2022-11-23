b4ae58bc0d4f28469632e17b3ca82d64b2b55697
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-23 09:55:53 +0900

    [Update] - Change elif -> if

--- baekjoon/dp/make_1.py
+++ baekjoon/dp/make_1.py
@@ -1,5 +1,4 @@
 n = int(input())
-# dp = [0] * (n + 1)
 dp = [0 for _ in range(n + 1)]
 
 for i in range(2, n + 1):
@@ -7,7 +6,7 @@ for i in range(2, n + 1):
 
     if i % 3 == 0:
         dp[i] = min(dp[i], dp[i // 3] + 1)
-    elif i % 2 == 0:
+    if i % 2 == 0:
         dp[i] = min(dp[i], dp[i // 2] + 1)
 
 print(dp[n])
