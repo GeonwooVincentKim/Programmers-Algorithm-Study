071ea94fb131aa44da07c4e9f6c9d0a65c841b18
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-06 10:41:16 +0900

    [Feature] - Not applied lambda function

---
+++ lambda_example/lambda_example.py
@@ -0,0 +1,14 @@
+def general_way(index):
+    return index * 3
+
+
+def solution(numbers):
+    get_list = list(map(str, numbers))
+    get_list.sort(key=general_way, reverse=True)
+    # get_list.sort(key=lambda x: x * 3, reverse=True)
+    return str(int(''.join(get_list)))
+
+
+if __name__ == "__main__":
+    print(solution([6, 10, 2]))
+    print(solution([3, 30, 34, 5, 9]))
