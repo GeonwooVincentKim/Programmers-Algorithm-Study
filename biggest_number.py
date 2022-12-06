807ac1c56b0252873ce6693faadb88ba737643e7
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-06 10:11:33 +0900

    [Feature] - Solution

---
+++ biggest_number.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    get_list = list(map(str, numbers))
+    get_list.sort(key=lambda x: x * 3, reverse=True)
+    return str(int(''.join(get_list)))
+
+
+if __name__ == "__main__":
+    print(solution([6, 10, 2]))
